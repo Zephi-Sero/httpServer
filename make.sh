1ae43fa4dfa010762388bb179fe795e8e57cdeae
Reese <Reesegetsemails@protonmail.com>
2022-09-10 05:21:14 -0600

    Oh wait that never happened oopsie

--- make.sh
+++ make.sh
@@ -4,10 +4,8 @@ mkdir -p ./bin
 
 WARNINGS="-Wall -Wextra -Wpedantic -Wabi"
 
-# This program uses my hashmap library available at: 
-# https://github.com/z4221/hashmap
 # tcc src/httpServer.c -o bin/httpServer
-musl-clang $WARNINGS -march=native -static -O3 src/httpServer.c -o bin/httpServer
+# musl-clang $WARNINGS -march=native -static -O3 src/httpServer.c -o bin/httpServer
 # clang -g $WARNINGS src/httpServer.c -o bin/httpServer
 # clang $WARNINGS -O3 src/httpServer.c -o bin/httpServer
 
