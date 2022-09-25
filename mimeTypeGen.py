4914405a95e859020d6123552a53d2b960b66a96
Zephi Sero <29110377+Zephi-Sero@users.noreply.github.com>
2022-09-25 08:36:10 -0500

    Fixed some strange not-very-pythonic things

--- mimeTypeGen.py
+++ mimeTypeGen.py
@@ -21,28 +21,22 @@ const mimeType mTypes[] = {
 
 """
 
-template = "	{\"{1}\",\"{2}\\r\\n\"},"
+template = "\t{\"{1}\",\"{2}\\r\\n\"},"
 
 print(base)
 
 
 
 for line in fullfile:
-	if line[0] == '#':
+	if len(line) == 0 or line[0] == '#':
 		continue
-	line = line.split()
-
-	try:
-		while 1:
-			line = line.remove('')
-	except:
-		pass
-
-	if line == [] or line == None:
+	
+	words = line.split()
+	if len(words) == 0:
 		continue
 
-	type = line.pop(0)
-	temp = template.replace("{2}",type);
-	for i in line:
+	type = line[0]
+	temp = template.replace("{2}",type)
+	for i in line[1:]:
 		print(temp.replace("{1}",i))
 print("};")
