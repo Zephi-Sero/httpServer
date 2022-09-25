a838be720c3d0c83af95d84d5641215fcba6d85e
Zephi Sero <29110377+Zephi-Sero@users.noreply.github.com>
2022-09-25 13:20:13 -0500

    Hopefully this fixes everything

--- mimeTypeGen.py
+++ mimeTypeGen.py
@@ -10,26 +10,34 @@ if len(argv) < 2:
 with open(argv[1]) as infile:
 	lines = infile.readlines()
 
-print("""
+finalStr = """
+#pragma once
+
+
 typedef struct {
-char const *const Extension;
-char const *const Type;
-} mimeType;
+\tchar const *const extension;
+\tchar const *const type;
+} MimeType;
+
 
 const mimeType mTypes[] = {
-""")
+
+"""
 
 for line in lines:
 	if len(line) == 0 or line[0] == '#':
 		continue
 
 	words = line.split()
-	if len(words) == 0:
+	if len(words) != 2:
+		if len(words) == 1:
+			print(f"No extension associated with MIME type '{words[0]}'")
 		continue
 
 	mimetype = words[0]
 	extension = words[1]
 
-	print(f"\t{{\"{extension}\", \"{mimetype}\\r\\n\"}},")
+	finalStr += f"\t{{\"{extension}\", \"{mimetype}\\r\\n\"}},\n"
 
-print("};")
+finalStr += "};"
+print(finalStr)
