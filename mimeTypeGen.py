db689c587483235540b09ec688bd61b6e13b7f45
Zephi Sero <29110377+Zephi-Sero@users.noreply.github.com>
2022-09-25 13:07:28 -0500

    Cleaned up

--- mimeTypeGen.py
+++ mimeTypeGen.py
@@ -1,42 +1,35 @@
 #!/bin/env python3
 # Changed this to add or remove mime types you want or dont want
 
-import sys
+from sys import argv
 
-if len(sys.argv) < 2:
+if len(argv) < 2:
 	print("Please provide a path to your mime.types file")
 	exit(1)
 
-infile = open(sys.argv[1])
-fullfile = infile.readlines()
-infile.close()
+with open(argv[1]) as infile:
+	lines = infile.readlines()
 
-base = """
+print("""
 typedef struct {
 char const *const Extension;
 char const *const Type;
 } mimeType;
 
 const mimeType mTypes[] = {
+""")
 
-"""
-
-template = "\t{\"{1}\",\"{2}\\r\\n\"},"
-
-print(base)
-
-
-
-for line in fullfile:
+for line in lines:
 	if len(line) == 0 or line[0] == '#':
 		continue
-	
+
 	words = line.split()
 	if len(words) == 0:
 		continue
 
-	type = words[0]
+	mimetype = words[0]
 	extension = words[1]
-	
-	print(f"\t{{\"{extension}\", \"{type}\\r\\n\"}},")
+
+	print(f"\t{{\"{extension}\", \"{mimetype}\\r\\n\"}},")
+
 print("};")
